6f3cde9da9fef1418a489a7f48eefac5bb387172
Rajal Kumar <rajalkumar@protonmail.com>
2025-07-22 23:41:51 +1200

    feat: create components folder and added global header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-grey-800 text-white p-4">
+      <h1 className="text-lg font-semibold">Booking Management App</h1>
+    </header>
+  );
+}
