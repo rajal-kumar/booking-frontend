5adfb901466fcffa267fc266aa6b98f186323877
Rajal Kumar <rajalkumar@protonmail.com>
2025-07-29 03:14:33 +1200

    feat: added back link to bookings

--- src/app/bookings/new/page.tsx
+++ src/app/bookings/new/page.tsx
@@ -1,10 +1,15 @@
 'use client'
 
+import Link from "next/link";
 import NewBookingForm from "@/components/NewBookingForm";
 
 export default function NewBookingPage() {
   return (
     <div className="max-w-xl mx-auto p-6">
+      <Link href="/bookings">
+        <button className="text-sm text-blue-500 hover:underline">← Back to Bookings</button>
+      </Link>
+      <br />
       <h1 className="text-3xl font-semibold text-blue-600 mb-4">
         Make a New Booking
       </h1>
